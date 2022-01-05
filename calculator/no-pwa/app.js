ffde122030aa14c71d10502da3434cfc6d41479b
castle-sky <familyboat@163.com>
2022-01-05 15:34:41 +0800

    Updates

---
+++ calculator/no-pwa/app.js
@@ -0,0 +1,40 @@
+window.addEventListener('load', () => {
+  const history = document.querySelector('#history > ul');
+  const current = document.querySelector('#current > p');  
+  const keyboard = document.querySelector('#keyboard');
+
+  let isOperator;
+
+  const getKeyboardValue = (element) => {
+    while (element.tagName !== 'SPAN') {
+      element = element.firstElementChild;
+    }
+    return element.textContent;
+  }
+
+  keyboard.addEventListener('click', (event) => {
+    const textContent = getKeyboardValue(event.target);
+
+    if (textContent !== '=') {
+      if (['+', '-', '*', '/', '.'].includes(textContent)) {
+        if (isOperator) {
+          return null;
+        } else {          
+          isOperator = true;
+        }
+      } else {
+        isOperator = false;
+      }
+      current.textContent += textContent;
+    } else {
+      if (current.textContent.trim() === '') {
+        return null;
+      }
+      const li = document.createElement('li');
+      li.textContent = `${current.textContent.trim()} = ${eval(current.textContent)}`;
+      history.appendChild(li);
+      li.scrollIntoView();
+      current.textContent = '';
+    }
+  });
+});
